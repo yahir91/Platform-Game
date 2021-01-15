8deb21372d618246e63262f0f52079c5483fecc8
Yahir Cardona <yahir.cardona@gmail.com>
2021-01-14 20:29:31 -0600

    testing with differente configurations

--- src/modules/Config/config.js
+++ src/modules/Config/config.js
@@ -1,8 +1,17 @@
 import 'phaser';
- 
-export default {
-  type: Phaser.AUTO,
-  parent: 'phaser-example',
-  width: 800,
-  height: 600
-};
+
+  // object containing configuration options
+  export default  {
+    type: Phaser.AUTO,
+    width: 1334,
+    height: 750,
+    
+    backgroundColor: 0x0c88c7,
+
+    // physics settings
+    physics: {
+        default: "arcade"
+    }
+      }
+  
+
